f4737fc7313dc348c61a3887dfc0c80e9dfe9fed
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-18 23:03:23 +0530

    Turbo sort

---
+++ cc_turbo_sort.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int  n;
+    cin>>n;
+    int arr[100000];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
+
