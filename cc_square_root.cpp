00e2133adf9eff168e88114711b294dbb7d52cf4
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-20 13:14:41 +0530

    Square root

---
+++ cc_square_root.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int n,s;
+	    cin>>n;
+	    s=sqrt(n);
+	    cout<<s<<endl;
+	}
+	return 0;
+}
