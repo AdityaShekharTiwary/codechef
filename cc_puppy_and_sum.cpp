84b36ba75c5f74221f4a98dbc4fa161448bd465b
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-21 15:18:20 +0530

    Puppy and sum
    https://www.codechef.com/problems/PPSUM

---
+++ cc_puppy_and_sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,d;
+        cin>>d>>n;
+        for(int i=0;i<d;i++)
+        {
+            n=n*(n+1)/2;
+        }
+        cout<<n<<endl;
+    }
+
+    return 0;
+}
