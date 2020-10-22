ac1c1fda9403c2f12962461035b2af5297797cb6
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-22 15:41:40 +0530

    The smallest pair
    https://www.codechef.com/problems/SMPAIR

---
+++ cc_the_smallest_pair.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[100000];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        cout<<arr[0]+arr[1]<<endl;
+    }
+
+}
