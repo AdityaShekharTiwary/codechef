7d0f01de4db449b50c80616cf8c2f5ec34ac73eb
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-12-28 00:08:34 +0530

    Sed sequence

---
+++ cc_sed_sequences.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[100];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        if(sum%k==0) cout<<0<<endl;
+        else cout<<1<<endl;
+
+    }
+    return 0;
+}
