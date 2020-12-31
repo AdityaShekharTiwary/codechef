14c96db7dc82014dfea05035d3fdf9bc9c29b40a
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-12-31 16:16:24 +0530

    cyberverse
    https://www.codechef.com/BYTR20B/problems/CYBV

---
+++ cc_cyberverse.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        cout<<k/n<<endl;
+    }
+    return 0;
+}
