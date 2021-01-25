a059edeb1a1f635d433ea841132ee4837095360b
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-25 19:05:02 +0530

    Distinct pair sum

---
+++ cc_distinct_pair_sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define v vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define m map<int,int>
+#define mll map<ll,ll>
+#define test int t; cin>>t; while(t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve()
+{
+    int full;
+
+}
+int main()
+{
+	fast
+	test
+	{
+        int l,r; cin>>l>>r;
+        cout<<2*r-2*l+1<<endl;
+	}
+
+	return 0;
+}
+
