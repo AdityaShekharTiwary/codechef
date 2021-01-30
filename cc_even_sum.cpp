5d7d5a6fc1957be62958e960d23561e97cf7fba4
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-30 23:23:08 +0530

    Even sum

---
+++ cc_even_sum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define v vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define m map<int,int>
+#define mll map<ll,ll>
+#define pb push_back
+#define test int t; cin>>t; while(t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve()
+{
+    int he;
+    int she;
+
+}
+
+int main()
+{
+	fast
+	test
+	{
+        ll n,sum=0; cin>>n;
+        vll arr(n);
+        input_arr
+        f(i,0,n) sum+=arr[i];
+        if(sum%2==0) cout<<1<<endl;
+        else cout<<2<<endl;
+
+	}
+
+	return 0;
+}
+
