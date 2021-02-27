e0beb26100a70a93f61589bbb36f4bc779e8add3
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-02-27 19:40:27 +0530

    Collect the bomb

---
+++ cc_collect_the_bomb.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vll vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define f2(i,s,e) for(int i=s;i<=e;i++)
+#define mi map<int,int>
+#define mp make_pair
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+void fast()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input1.txt","r",stdin);
+		freopen("output1.txt","w",stdout);
+	#endif
+
+	fast_IO
+	cout.tie(NULL);
+}
+
+
+
+void solve()
+{
+	string s; cin>>s;
+	int cnt=0;
+	int n=s.size();
+	f(i,0,n) {
+		if(s[i]== '#') cnt++;
+	}
+	cout<<cnt<<endl;
+	  
+}
+
+	
+
+int main()
+{
+	fast();
+
+	//test
+	{
+		solve();
+	}
+
+	return 0;
+}
