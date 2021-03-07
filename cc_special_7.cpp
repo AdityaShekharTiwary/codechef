e5119aa0ed24da1db56bb52ed1ee2cd306280614
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-03-07 21:07:55 +0530

    Special 7

---
+++ cc_special_7.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define input_arr for(ll i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(ll i=s;i<e;i++)
+#define fo(i,s,e) for(ll i=s;i<=e;i++)
+#define mi map<int,int>
+#define mp make_pair
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+void fast()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input1.txt","r",stdin);
+		freopen("output1.txt","w",stdout);
+	#endif
+
+	fast_IO
+	cout.tie(NULL);
+}
+
+
+
+void solve()
+{
+	string s; cin>>s;
+	int n=s.size();
+	bool f=false;
+	f(i,0,n) {
+		if(s[i]=='7') f=true;
+	}
+	if(f) cout<<"True\n";
+	else cout<<"False\n";
+
+}
+
+
+
+int main()
+{
+	fast();
+
+	test
+	{
+		solve();
+	}
+
+	return 0;
+}
