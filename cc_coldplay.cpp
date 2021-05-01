cd5c9d80343fffef3ddab34927630de2246a9b1b
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-01 19:27:49 +0530

    Cold play

---
+++ cc_coldplay.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(ll i=s;i<e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	fast_IO
+	cout.tie(NULL);
+}
+
+void solve()
+{
+	int n, m; cin >> n >> m;
+	cout << n / m << endl;
+
+
+}
+
+
+int main()
+{
+	fast();
+
+	test
+	{
+		solve();
+	}
+
+	return 0;
+}
