7803b9b13128f17cc17a4562f43cd39ab75a7bb6
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-06-01 14:32:20 +0530

    Birthday gifts

---
+++ birthday_gists.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long int
+#define pb push_back
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#define f(i,s,e) for(ll i=s;i<e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+#endif
+
+    fast_IO
+    cout.tie(NULL);
+}
+
+const ll mod = 1e9 + 7;
+
+void solve()
+{
+    ll n, k; cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end(), greater<int>());
+
+    ll first = 0, second = 0;
+    for (int i = 0; i < 2 * k; i += 2) {
+        first += arr[i];
+        second += arr[i + 1];
+    }
+    second += arr[2 * k];
+    cout << max(first, second) << endl;
+}
+
+
+
+int main()
+{
+    fast();
+
+    test
+    {
+        solve();
+    }
+
+    return 0;
+}
