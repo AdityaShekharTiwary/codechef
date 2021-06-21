9979890978843171c5fab409d2a6c1e07f00103d
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-06-21 22:51:47 +0530

    Chefland and Visa

---
+++ chefland_and_visa.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long int
+#define pb push_back
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#define f(i,s,e) for(ll i=s;i<e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+const ll mod = 1e9 + 7;
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+#endif
+
+    fast_IO
+    cout.tie(NULL);
+}
+
+
+void solve()
+{
+
+    int x1, y1, z1, x2, y2, z2; cin >> x1 >> x2 >> y1 >> y2 >> z1 >> z2;
+    bool f1 = false, f2 = false, f3 = false;
+    if (x2 >= x1) f1 = true;
+    if (y2 >= y1) f2 = true;
+    if (z2 <= z1) f3 = true;
+
+    debug(f1)
+    debug(f2)
+    debug(f3)
+    if (f1 && f2 && f3) cout << "YES\n";
+    else cout << "NO\n";
+
+}
+
+
+
+int main()
+{
+    fast();
+
+    test
+    {
+        solve();
+    }
+
+    return 0;
+}
