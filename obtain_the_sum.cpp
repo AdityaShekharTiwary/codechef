6a1d2301ee331c2f854e530e7c0dd041cc04a3cf
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-09-16 09:00:48 +0530

    Obtain the sum

---
+++ obtain_the_sum.cpp
@@ -0,0 +1,85 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long int
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define nline "\n"
+#define all(x) (x).begin(), (x).end()
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#define rep(i,s,e) for(int i=s;i<e;i++)
+#define repp(i,s,e) for(int i=s;i<=e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+const ll mod = 1e9 + 7;
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("debug.txt", "w", stderr);
+#endif
+
+	fast_IO
+	cout.tie(NULL);
+}
+
+void solve()
+{
+	ll n, s;
+	cin >> n >> s;
+	ll sum = (n * (n + 1)) / 2;
+	if (sum - s > 0 && sum - s <= n) {
+		cout << sum - s << endl;
+	}
+	else cout << "-1\n";
+
+}
+
+
+
+int main()
+{
+	fast();
+
+	test
+
+	{
+		solve();
+	}
+
+
+
+	return 0;
+}
+
